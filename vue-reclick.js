1175fd815838c0f9bd96bc5b46f42522168f39ab
csh <webfansplz@kobe.com>
2017-12-21 17:44:03 +0800

    hello world

---
+++ vue-reclick.js
@@ -0,0 +1,55 @@
+(function() {
+  /**
+   * 函数节流
+   *
+   * @param {any} method
+   */
+  function throttle(method) {
+    clearTimeout(method.tId);
+    method.tId = setTimeout(function() {
+      method.call();
+    }, 200);
+  }
+  /**
+   * 事件绑定
+   *
+   * @param {any} element  绑定dom
+   * @param {any} event    事件类型
+   * @param {any} listener 方法
+   */
+  function addEvent(element, event, listener) {
+    if (element.addEventListener) {
+      element.addEventListener(event, listener, false);
+    } else if (element.attachEvent) {
+      element.attachEvent('on' + event, listener);
+    } else {
+      element['on' + event] = listener;
+    }
+  }
+  var vueReclick = {};
+  var reclick = {
+    bind: function(el, binding) {
+      addEvent(el, 'click', function() {
+        throttle(binding.value);
+      });
+    },
+    unbind: function(el) {
+      addEvent(el, 'click', function() {});
+    }
+  };
+
+  vueReclick.install = function(Vue) {
+    Vue.directive('reclick', reclick);
+  };
+
+  if (typeof exports == 'object') {
+    module.exports = vueReclick;
+  } else if (typeof define == 'function' && define.amd) {
+    define([], function() {
+      return vueReclick;
+    });
+  } else if (window.Vue) {
+    window.vueReclick = vueReclick;
+    Vue.use(vueReclick);
+  }
+})();
